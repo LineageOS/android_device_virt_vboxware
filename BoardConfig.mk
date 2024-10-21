4cbe18c7e8e7546fffb5b124db827b6409cee261
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-21 20:51:48 +0800

    [WIP] vboxware: Bringup 3D accelerated graphics
    Change-Id: Ife8cacf7888c988e4eb7ae49c81764708ab237be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,22 +32,21 @@ else
 $(call soong_config_set,VBOXWARE_FSTAB,PARTITION_SCHEME,a)
 endif
 
+# Graphics (Mesa)
+BOARD_GPU_DRIVERS := vmwgfx
+BOARD_USES_DRM_GRALLOC := true
+
 # GRUB
 TARGET_GRUB_ARCH := x86_64-efi
 
 # Kernel
 BOARD_KERNEL_CMDLINE += \
     8250.nr_uarts=1 \
+    console=tty0 \
     androidboot.console=ttyS0 \
-    androidboot.graphics=swiftshader \
     androidboot.hardware=vboxware \
-    androidboot.partition_map=sdb,userdata
-
-BOARD_KERNEL_CMDLINE_BOOT := \
-    nomodeset
-
-BOARD_KERNEL_CMDLINE_RECOVERY := \
-    console=tty0
+    androidboot.partition_map=sdb,userdata \
+    audit=0 androidboot.selinux=permissive androidboot.insecure_adb=1
 
 BOARD_KERNEL_IMAGE_NAME := bzImage
 
--- device.mk
+++ device.mk
@@ -15,7 +15,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    gralloc.drm
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.gralloc=drm
+
+# Graphics (Mesa)
+PRODUCT_PACKAGES += \
+    libGLES_mesa
+
+PRODUCT_SOONG_NAMESPACES += \
+    external/mesa3d
 
 # Init
 PRODUCT_COPY_FILES += \
