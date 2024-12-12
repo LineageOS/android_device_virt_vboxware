2fe88ae8a2081072c9781710b5ca058111b37af0
Jihoon Kang <jihoonkang@google.com>
2024-12-12 02:05:56 +0000

    vboxware: Convert TARGET_RECOVERY_FSTAB_GENRULE to PRODUCT_PACKAGES
    TARGET_RECOVERY_FSTAB_GENRULE is used to specify the name of the genrule
    module, which is then installed to the recovery partition. Instead of
    such logic, prebuilt_* module can be utilized to install the module in
    the correct install path.
    
    [cafebabe: Adapt]
    
    Test: build recovery image and verify install paths do not change
    Bug: 381888358
    Change-Id: Ie94541cdcfcab2c69e52db1876e77fa9f329d2de

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,6 @@ $(call soong_config_set,VIRT_PREINSTALL_CHECK,DRM_CARD_NAME,vmwgfx)
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_FSTAB_GENRULE := gen_fstab_vboxware
 TARGET_RECOVERY_PIXEL_FORMAT := BGRX_8888
 
 # SELinux
