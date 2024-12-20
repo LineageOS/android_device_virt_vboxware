40374d34b91bf9e396a595ba8ad0c331dfed052e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-21 07:12:55 +0800

    [DNM] vboxware: Inherit virtio_x86_64 proprietary files (15 QPR1)
    Change-Id: Ibf147efaacfcfeca68894b5e62cdad09fe629a56

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,9 @@
 TARGET_BOOT_MANAGER := grub
 
 # Inherit from common
+WITH_GMS := true
 include device/virt/virt-common/BoardConfigVirtCommon.mk
+WITH_GMS :=
 
 USES_DEVICE_VIRT_VBOXWARE := true
 
@@ -24,6 +26,9 @@ TARGET_GRUB_BOOT_CONFIGS += $(DEVICE_PATH)/bootmgr/grub/grub-boot.cfg
 TARGET_GRUB_INSTALL_CONFIGS += $(DEVICE_PATH)/bootmgr/grub/grub-install.cfg
 TARGET_REFIND_BOOT_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-boot.conf
 TARGET_REFIND_INSTALL_CONFIG := $(DEVICE_PATH)/bootmgr/rEFInd/refind-install.conf
+
+# Build
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Fstab
 ifeq ($(AB_OTA_UPDATER),true)
@@ -55,6 +60,12 @@ else ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/kernel),)
 BOARD_VENDOR_KERNEL_MODULES := \
     $(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/*.ko)
 endif
+
+# Native bridge
+TARGET_NATIVE_BRIDGE_ARCH := arm64
+TARGET_NATIVE_BRIDGE_ARCH_VARIANT := armv8-a
+TARGET_NATIVE_BRIDGE_CPU_VARIANT := generic
+TARGET_NATIVE_BRIDGE_ABI := arm64-v8a
 
 # Recovery
 TARGET_RECOVERY_FSTAB_GENRULE := gen_fstab_vboxware
--- device.mk
+++ device.mk
@@ -9,6 +9,19 @@ $(call inherit-product, device/virt/virt-common/virt-common.mk)
 
 DEVICE_PATH := device/virt/vboxware
 
+# Google Apps
+PRODUCT_PACKAGES += \
+    GmsOverlay \
+    GmsSettingsOverlay \
+    GmsSettingsProviderOverlay \
+    GmsSetupWizardOverlay
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.theme=glif_v3_light \
+    ro.opa.eligible_device=true \
+    ro.com.google.gmsversion=14_beta
+
 # Graphics (Composer)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
@@ -49,6 +62,23 @@ else ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/kernel),)
     $(warning Using prebuilt kernel from $(TARGET_PREBUILT_KERNEL_DIR)/kernel)
 endif
 
+# Native bridge
+include frameworks/libs/native_bridge_support/native_bridge_support.mk
+
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/libs/native_bridge_support/libc
+
+PRODUCT_PACKAGES += \
+    $(NATIVE_BRIDGE_PRODUCT_PACKAGES) \
+    libberberis_exec_region
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.dalvik.vm.native.bridge=libndk_translation.so \
+    ro.dalvik.vm.isa.arm64=x86_64 \
+    ro.dalvik.vm.isa.arm=x86 \
+    ro.enable.native.bridge.exec=1 \
+    ro.ndk_translation.version=0.2.3
+
 # Recovery
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/init/init.recovery.vboxware.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.vboxware.rc
@@ -64,3 +94,6 @@ PRODUCT_PACKAGES += \
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
+
+# Inherit from proprietary files
+$(call inherit-product, vendor/virt/virtio_x86_64/virtio_x86_64-vendor.mk)
